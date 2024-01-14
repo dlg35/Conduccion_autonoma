d5b6fd8a517469f9c790b2c696d69f942de2dca8
Dario Lopez-Davalos Gomez <dlg35@alu.ua.es>
2024-01-14 19:33:54 +0100

    Todo terminado

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/dario/Escritorio/moviles_proyecto/src
+# Build directory: /home/dario/Escritorio/moviles_proyecto/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("navigation_stage")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/dario/Escritorio/moviles_proyecto/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/noetic/bin:/home/dario/androidSDK/tools/:/home/dario/androidSDK/tools/bin:/home/dario/miniconda3/condabin:/home/dario/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PWD='/home/dario/Escritorio/moviles_proyecto/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/dario/Escritorio/moviles_proyecto/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/dario/Escritorio/moviles_proyecto/src:$ROS_PACKAGE_PATH"
